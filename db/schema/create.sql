386684e27a6f4e1794c9eb637a189544c8cb7801
TeddyGavi <matc.davis@gmail.com>
2023-01-24 15:13:23 -0800

    continue working on schema issue with generate statement

--- db/schema/create.sql
+++ db/schema/create.sql
@@ -2,11 +2,21 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS expenditures CASCADE;
 DROP TABLE IF EXISTS currencies CASCADE;
 DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS months CASCADE;
 
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  monthly_budget INTEGER
+);
 
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
   name TEXT NOT NULL
 );
 
@@ -18,25 +28,23 @@ CREATE TABLE currencies (
   rate_to_usd NUMERIC(2)
 );
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  monthly_budget INTEGER
-);
-
-
 CREATE TABLE expenditures (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   currency_id INTEGER REFERENCES currencies(id) ON DELETE CASCADE,
   cost NUMERIC(2) NOT NULL,
-  exchange_rate_base NUMERIC(8)
-  cost_in_base GENERATED ALWAYS AS (cost * exchange_rate_base) STORED,
+  exchange_rate_base NUMERIC(8) NOT NULL,
+  cost_in_base NUMERIC, 
+  -- GENERATED ALWAYS as (cost * exchange_rate_base) STORED,
   date_paid DATE NOT NULL,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   notes TEXT
 );
+
+CREATE TABLE months (
+  id SERIAL PRIMARY KEY NOT NULL,
+  year INTEGER NOT NULL,
+  month INTEGER NOT NULL,
+  number_of_days INTEGER DEFAULT(30)
+)
 
