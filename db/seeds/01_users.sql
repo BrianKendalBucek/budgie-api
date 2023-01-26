c92cbbb12ee6e9aa0ff92b010216ad4ab7938377
Matthew Davis <102203363+TeddyGavi@users.noreply.github.com>
2023-01-26 09:45:52 -0800

    Update 01_users.sql
    OOPS hot fix the hot break

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,1 +1,1 @@
-INSERT INTO users(first_name, last_name, email, password, currency_id, monthly_budget) VALUES
+INSERT INTO users(first_name, last_name, email, password, currency_id, monthly_budget) VALUES ($1, $2, $3, $4, $5, $6);
