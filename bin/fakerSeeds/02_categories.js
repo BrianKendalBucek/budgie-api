5945df482e59c38044542b7297e6711e20f859f2
Bryce Haley <bhaley91@gmail.com>
2023-02-07 08:07:20 -0500

    finish static demo data for db:reset

--- bin/fakerSeeds/02_categories.js
+++ bin/fakerSeeds/02_categories.js
@@ -1,14 +1,29 @@
 const { faker } = require("@faker-js/faker");
 const db = require("../../db/connection");
 
-const categories = async (sql, seedLength) => {
-  for (let i = 0; i < seedLength; i++) {
-    const params = [
-      faker.datatype.number({ min: 1, max: seedLength }),
-      faker.commerce.product(),
+const categories = async (random, sql, seedLength) => {
+  if (random) {
+    for (let i = 0; i < seedLength; i++) {
+      const params = [
+        faker.datatype.number({ min: 1, max: seedLength }),
+        faker.commerce.product(),
     ];
     await db.query(sql, params);
   }
+} else {
+  const params = [
+    [1, 'toys'],
+    [1, 'beer'],
+    [1, 'yarn'],
+    [2, 'hoodies'],
+    [2, 'hats'],
+    [2, 'childcare'],
+    [3, 'tropical paradise things']
+  ];
+  for (let param of params) {
+    await db.query(sql, param);
+  }
+}
 };
 
 module.exports = categories;
