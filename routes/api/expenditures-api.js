8e092fa889d77fd236539c711c743d7cf883a13b
TeddyGavi <matc.davis@gmail.com>
2023-02-08 20:09:13 -0800

    adjust expense query to show date in easer to work with format

--- routes/api/expenditures-api.js
+++ routes/api/expenditures-api.js
@@ -55,12 +55,12 @@ router.post("/", (req, res) => {
 router.get("/totals_per_day", (req, res) => {
   const userId = req.session.user;
   EQueries.getTotalPerDay(userId)
-  .then((totals) => {
-    res.json(totals)
-  })
-  .catch((err) => {
-    res.status(500).json({ error: err.message });
-  });
+    .then((totals) => {
+      res.json(totals);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
 // TODO: think about response on delete
