57bb6e6e6a6a8325d373efd83195acb8e73aa76c
TeddyGavi <matc.davis@gmail.com>
2023-02-13 13:54:24 -0800

    edite expense query to return first created expense

--- db/queries/expenditures.js
+++ db/queries/expenditures.js
@@ -20,7 +20,7 @@ const getAllExpenditures = () => {
 };
 
 const getAllExpendituresByUserIdJoinCurrencies = (id) => {
-  const sql = `SELECT e.id as ex_id, e.user_id, e.currency_id as paid_in_curr_id, e.cost, e.exchange_rate_base, TO_CHAR(e.date_paid :: DATE, 'yyyy/mm/dd') as date_paid, e.category_id, e.notes, c.id as curr_id, c.name as curr_name, c.code as code, c.rate_to_usd  FROM expenditures e JOIN currencies c ON e.currency_id=c.id WHERE user_id=$1;`;
+  const sql = `SELECT e.id as ex_id, e.user_id, e.currency_id as paid_in_curr_id, e.cost, e.exchange_rate_base, TO_CHAR(e.date_paid :: DATE, 'yyyy/mm/dd') as date_paid, e.category_id, e.notes, c.id as curr_id, c.name as curr_name, c.code as code, c.rate_to_usd  FROM expenditures e JOIN currencies c ON e.currency_id=c.id WHERE user_id=$1 ORDER BY e.id DESC;`;
   const params = [id];
   return db.query(sql, params).then((data) => data.rows);
 };
@@ -31,16 +31,37 @@ const getOneExpenditureById = (id) => {
   return db.query(sql, params).then((data) => data.rows);
 };
 
-const createNewExpenditure = (currency_id, cost, date_paid, categoryId, notes, userId) => {
-  return db.query(`SELECT TRUNC(user_c.rate_to_usd * ( 1 / expense_c.rate_to_usd), 6) AS exchange_rate_base 
+const createNewExpenditure = (
+  currency_id,
+  cost,
+  date_paid,
+  categoryId,
+  notes,
+  userId
+) => {
+  return db
+    .query(
+      `SELECT TRUNC(user_c.rate_to_usd * ( 1 / expense_c.rate_to_usd), 6) AS exchange_rate_base 
                       FROM users u 
                         JOIN currencies user_c ON u.id = $1 AND u.currency_id = user_c.id 
-                        JOIN currencies expense_c ON expense_c.id = $2;`, [userId, currency_id])
+                        JOIN currencies expense_c ON expense_c.id = $2;`,
+      [userId, currency_id]
+    )
     .then((data) => {
-      exchange_rate_base = data.rows[0].exchange_rate_base
-      const sql = `INSERT INTO expenditures(user_id, currency_id, cost, exchange_rate_base, date_paid, category_id, notes) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`
-      params = [ userId, currency_id, cost, exchange_rate_base, date_paid, categoryId, notes ];
-      db.query(sql, params).then((data) => {data.rows[0]});
+      exchange_rate_base = data.rows[0].exchange_rate_base;
+      const sql = `INSERT INTO expenditures(user_id, currency_id, cost, exchange_rate_base, date_paid, category_id, notes) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`;
+      params = [
+        userId,
+        currency_id,
+        cost,
+        exchange_rate_base,
+        date_paid,
+        categoryId,
+        notes,
+      ];
+      db.query(sql, params).then((data) => {
+        data.rows[0];
+      });
     });
 };
 
@@ -62,7 +83,7 @@ const getTotalPerDay = (userId) => {
   ORDER BY date_paid;`;
   const params = [userId];
   return db.query(sql, params).then((data) => data.rows);
-}; 
+};
 
 const getBudgetPercentage = (userId) => {
   const sql = `SELECT u.id as user_id, 
@@ -76,12 +97,9 @@ const getBudgetPercentage = (userId) => {
                 ON u.id = $1 AND u.id = e.user_id AND 
                       EXTRACT(MONTH FROM e.date_paid) = EXTRACT(MONTH FROM NOW()) AND  
                       EXTRACT(YEAR FROM e.date_paid) = EXTRACT(YEAR FROM NOW())
-                GROUP BY u.id, u.monthly_budget;`
+                GROUP BY u.id, u.monthly_budget;`;
   return db.query(sql, [userId]).then((data) => data.rows[0]);
-                    
-}
-
-
+};
 
 const getTotalPerMonth = (userId) => {
   const sql = `SELECT
@@ -94,7 +112,6 @@ const getTotalPerMonth = (userId) => {
   return db.query(sql, params).then((data) => data.rows);
 };
 
-
 module.exports = {
   getAllExpendituresByUserIdJoinCurrencies,
   deleteExpenditureById,
@@ -105,6 +122,5 @@ module.exports = {
   getOneExpenditureById,
   getTotalPerDay,
   getTotalPerMonth,
-  getBudgetPercentage
+  getBudgetPercentage,
 };
-
