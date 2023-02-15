d918594fa89449b21a88718f5aa328e8b4ef407c
TeddyGavi <matc.davis@gmail.com>
2023-02-14 21:33:27 -0800

    remove console logs and clean up code of not needed comments

--- routes/authenticateAPI.js
+++ routes/authenticateAPI.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const UQueries = require("../db/queries/users");
 
 router.use("/", async (req, res, next) => {
-  // is there a logged in user?
-  // double check the db
-  // continue to next api request
-  console.log("all api routes go through ME!!!!!!");
-
+  console.log("API route auth...");
 
   if (!req.session.user) {
     res.status(401).json("Not logged in");
@@ -22,7 +18,6 @@ router.use("/", async (req, res, next) => {
         next();
       }
     } catch (error) {
-      console.log(error);
       res.status(500);
     }
   }
